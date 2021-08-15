ceab124fa83c7d34cd596c4b5357ce80ea2f1785
VioletViola <vivian.v18723@gmail.com>
2021-08-15 16:49:53 +0100

    Produce different data types in java.

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,13 +1,8 @@
 public class DataTypes {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         System.out.println(5);
         System.out.println(11.2);
+        System.out.println("Hello World!");
         System.out.println(true);
-        System.out.println(10 % 7);
-        System.out.println(7 * 7);
-        System.out.println(7 / 7);
-        System.out.println(7 - 7);
-        System.out.println(7 + 7);
     }
 }
